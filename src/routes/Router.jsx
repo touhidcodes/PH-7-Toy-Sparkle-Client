8942917db6768349871e74fffcfb9250f0f733d7
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-18 02:49:50 +0600

    install-tailwind-daisyui

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home/Home";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <MainLayout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+		],
 	},
 ]);
 
