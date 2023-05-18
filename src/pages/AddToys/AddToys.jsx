c69000b90a5164c5d0bc2feca168a288ae45231f
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-19 03:47:33 +0600

    add-toy-page

--- src/pages/AddToys/AddToys.jsx
+++ src/pages/AddToys/AddToys.jsx
@@ -9,12 +9,55 @@ const AddToys = () => {
 			</div>
 			<div className='w-1/2 mx-auto mt-10'>
 				<div className=' card  p-10 rounded-xl shadow-2xl bg-base-100'>
-					<div className='card-body border-dashed border-2 border-gray-200'>
+					<div className='card-body border-dashed border-2 border-[#4acdd5]'>
 						<div className='form-control'>
 							<label className='label flex flex-row justify-start'>
 								<span className='text-gray-500 font-semibold'>
-									E-MAIL ADDRESS:{" "}
-								</span>{" "}
+									TOY IMAGE URL:
+								</span>
+								<span className='text-red-500 font-bold ml-2'>*</span>
+							</label>
+							<input
+								type='text'
+								placeholder='Image URL'
+								name='url'
+								className='bg-base-200 py-1 px-5 rounded-md  outline-offset-4 outline-2 outline-gray-400'
+								required
+							/>
+						</div>
+						<div className='form-control'>
+							<label className='label flex flex-row justify-start'>
+								<span className='text-gray-500 font-semibold'>TOY NAME:</span>
+								<span className='text-red-500 font-bold ml-2'>*</span>
+							</label>
+							<input
+								type='text'
+								placeholder='Toy Name'
+								name='name'
+								className='bg-base-200 py-1 px-5 rounded-md  outline-offset-4 outline-2 outline-gray-400'
+								required
+							/>
+						</div>
+						<div className='form-control'>
+							<label className='label flex flex-row justify-start'>
+								<span className='text-gray-500 font-semibold'>
+									SELLER NAME:
+								</span>
+								<span className='text-red-500 font-bold ml-2'>*</span>
+							</label>
+							<input
+								type='text'
+								placeholder='Seller Name'
+								name='sellerName'
+								className='bg-base-200 py-1 px-5 rounded-md  outline-offset-4 outline-2 outline-gray-400'
+								required
+							/>
+						</div>
+						<div className='form-control'>
+							<label className='label flex flex-row justify-start'>
+								<span className='text-gray-500 font-semibold'>
+									E-MAIL ADDRESS:
+								</span>
 								<span className='text-red-500 font-bold ml-2'>*</span>
 							</label>
 							<input
@@ -26,22 +69,82 @@ const AddToys = () => {
 							/>
 						</div>
 						<div className='form-control'>
-							<label className='label'>
-								<span className='label-text'>Password</span>
+							<label className='label flex flex-row justify-start'>
+								<span className='text-gray-500 font-semibold'>
+									SUB-CATEGORY:
+								</span>
+								<span className='text-red-500 font-bold ml-2'>*</span>
+							</label>
+							<input
+								type='email'
+								placeholder='Email Address'
+								name='email'
+								className='bg-base-200 py-1 px-5 rounded-md  outline-offset-4 outline-2 outline-gray-400'
+								required
+							/>
+						</div>
+						<div className='form-control'>
+							<label className='label flex flex-row justify-start'>
+								<span className='text-gray-500 font-semibold'>PRICE:</span>
+								<span className='text-red-500 font-bold ml-2'>*</span>
 							</label>
 							<input
 								type='text'
-								placeholder='password'
-								className='input input-bordered'
+								placeholder='Price'
+								name='price'
+								className='bg-base-200 py-1 px-5 rounded-md  outline-offset-4 outline-2 outline-gray-400'
+								required
 							/>
-							<label className='label'>
-								<a href='#' className='label-text-alt link link-hover'>
-									Forgot password?
-								</a>
+						</div>
+						<div className='form-control'>
+							<label className='label flex flex-row justify-start'>
+								<span className='text-gray-500 font-semibold'>RATING:</span>
+								<span className='text-red-500 font-bold ml-2'>*</span>
 							</label>
+							<input
+								type='text'
+								placeholder='Rating'
+								name='rating'
+								className='bg-base-200 py-1 px-5 rounded-md  outline-offset-4 outline-2 outline-gray-400'
+								required
+							/>
+						</div>
+						<div className='form-control'>
+							<label className='label flex flex-row justify-start'>
+								<span className='text-gray-500 font-semibold'>
+									AVAILABLE QUANTITY:
+								</span>
+								<span className='text-red-500 font-bold ml-2'>*</span>
+							</label>
+							<input
+								type='text'
+								placeholder='Available Quantity'
+								name='quantity'
+								className='bg-base-200 py-1 px-5 rounded-md  outline-offset-4 outline-2 outline-gray-400'
+								required
+							/>
+						</div>
+						<div className='form-control'>
+							<label className='label flex flex-row justify-start'>
+								<span className='text-gray-500 font-semibold'>
+									DESCRIPTION:
+								</span>
+								<span className='text-red-500 font-bold ml-2'>*</span>
+							</label>
+							<input
+								type='textbox'
+								placeholder='Write Toy Description'
+								name='rating'
+								className='bg-base-200 px-5 h-20 rounded-md  outline-offset-4 outline-2 outline-gray-400'
+								required
+							/>
+						</div>
+						<div className='flex flex-row text-red-500 font-semibold mt-2'>
+							<span className=' mr-2'>*</span>
+							<p>Fields are required</p>
 						</div>
 						<div className='form-control mt-6'>
-							<button className='btn btn-primary'>Login</button>
+							<button className='btn btn-error text-white'>ADD TOY </button>
 						</div>
 					</div>
 				</div>
