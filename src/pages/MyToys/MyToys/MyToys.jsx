e3f3f08d0a982fb3e907f849a484a49aafbb61ae
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-19 23:55:58 +0600

    my-toy-structure-display

--- src/pages/MyToys/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys/MyToys.jsx
@@ -1,17 +1,46 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../context/AuthProvider";
+import MyToysCard from "../MyToysCard/MyToysCard";
 
 const MyToys = () => {
 	const { user } = useContext(AuthContext);
+	const [myToys, setMyToys] = useState([]);
 	const url = `http://localhost:5000/my?email=${user.email}`;
 
-	console.log(user);
 	useEffect(() => {
 		fetch(url)
 			.then((res) => res.json())
-			.then((data) => console.log(data));
+			.then((data) => setMyToys(data));
 	}, []);
-	return <div>My toys</div>;
+
+	console.log(myToys);
+	return (
+		<div className='overflow-x-auto w-full mt-10'>
+			<div className='text-center'>
+				<h1 className='text-3xl font-semibold'>My Toys</h1>
+				<p className='text-[#4acdd5] font-semibold mt-1'>{user.email}</p>
+			</div>
+			<table className='table w-full mt-10'>
+				{/* head */}
+				<thead>
+					<tr>
+						
+						<th>Toy Name</th>
+						<th>Seller</th>
+						<th>Sub_category</th>
+						<th>Price & Quantity</th>
+						<th>Update Info</th>
+						<th>Delete Toy</th>
+					</tr>
+				</thead>
+				<tbody>
+					{myToys.map((toy) => (
+						<MyToysCard key={toy._id} toy={toy} />
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default MyToys;
--- src/pages/MyToys/MyToysCard/MyToysCard.jsx
+++ src/pages/MyToys/MyToysCard/MyToysCard.jsx
@@ -1,7 +1,51 @@
+import { Rating } from "@smastrom/react-rating";
 import React from "react";
+import { Link } from "react-router-dom";
 
-const MyToysCard = () => {
-	return <div></div>;
+const MyToysCard = ({ toy }) => {
+	const {
+		_id,
+		picture,
+		toyName,
+		price,
+		quantity,
+		rating,
+		sellerName,
+		category,
+	} = toy;
+	console.log(toy);
+	return (
+		<tr>
+			<td>
+				<div className='flex items-center space-x-3'>
+					<div className='avatar'>
+						<div className='mask mask-squircle w-16 h-16'>
+							<img src={picture} alt='car toy' />
+						</div>
+					</div>
+					<div>
+						<div className='font-bold'>{toyName}</div>
+						<div className='text-sm opacity-50'>
+							<Rating style={{ maxWidth: 100 }} value={rating} readOnly />
+						</div>
+					</div>
+				</div>
+			</td>
+
+			<td>{sellerName}</td>
+			<td>{category}</td>
+			<td>
+				<p>Price: {price}</p>
+				<p>Quantity: {quantity}</p>
+			</td>
+			<th>
+				<button className='btn btn-info text-white '>Update</button>
+			</th>
+			<th>
+				<button className='btn btn-error text-white '>Delete</button>
+			</th>
+		</tr>
+	);
 };
 
 export default MyToysCard;
